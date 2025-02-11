ee346c8fff89d52c980d8693cd96b6053f1ad062
BaiYang2238 <142558485+BaiYang2238@users.noreply.github.com>
2025-02-11 19:54:35 +0800

    [change] Optimized UI

--- Ink Canvas/Properties/AssemblyInfo.cs
+++ Ink Canvas/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Ink Canvas Better")]
-[assembly: AssemblyCopyright("Copyright © XY Wang 2023 & Chang Wu 2024")]
+[assembly: AssemblyCopyright("Copyright © XY Wang 2023 & Chang Wu 2024 & Bai Yang 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.2")]
-[assembly: AssemblyFileVersion("1.3.0.2")]
+[assembly: AssemblyVersion("1.3.0.3")]
+[assembly: AssemblyFileVersion("1.3.0.3")]
