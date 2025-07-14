9029f9a796d83c20eca020ca597539b28a7628fc
BaiYang2238 <142558485+BaiYang2238@users.noreply.github.com>
2025-07-14 14:45:27 +0800

    [refactor] part 13

--- Ink-Canvas-Better/Controls/ICB_PresetColor.xaml.cs
+++ Ink-Canvas-Better/Controls/ICB_PresetColor.xaml.cs
@@ -46,11 +46,63 @@ namespace Ink_Canvas_Better.Controls
 
         #endregion
 
+        #region IsSelected
+
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register(
+                "IsSelected",
+                typeof(bool),
+                typeof(ICB_PresetColor),
+                new PropertyMetadata(IsSelected_OnValueChanged)
+            );
+
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+        private static void IsSelected_OnValueChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            var control = (ICB_PresetColor)dependencyObject;
+            if ((bool)eventArgs.NewValue)
+            {
+                control.Viewbox1.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                control.Viewbox1.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Events
+
+        public static readonly RoutedEvent ColorSelectedEvent =
+            EventManager.RegisterRoutedEvent(
+                "ColorSelected",
+                RoutingStrategy.Bubble,
+                typeof(RoutedEventHandler),
+                typeof(ICB_PresetColor));
+
+        public event RoutedEventHandler ColorSelected
+        {
+            add => AddHandler(ColorSelectedEvent, value);
+            remove => RemoveHandler(ColorSelectedEvent, value);
+        }
+
         #endregion
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MainWindow.DrawingAttributes.Color = ((SolidColorBrush)InnerBorder.Background).Color;
+            
+            var args = new RoutedEventArgs(ColorSelectedEvent, this);
+            RaiseEvent(args);
+            e.Handled = true;
         }
     }
 }
--- Ink-Canvas-Better/Windows/FloatingBarIcons/FloatingBar_Pen.xaml.cs
+++ Ink-Canvas-Better/Windows/FloatingBarIcons/FloatingBar_Pen.xaml.cs
@@ -1,13 +1,12 @@
-﻿using System;
+﻿using Ink_Canvas_Better.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Ink;
 using System.Windows.Media;
-using Ink_Canvas_Better;
 
 namespace Ink_Canvas_Better.Windows.FloatingBarIcons
 {
@@ -19,6 +18,15 @@ namespace Ink_Canvas_Better.Windows.FloatingBarIcons
         public FloatingBar_Pen()
         {
             InitializeComponent();
+
+            AddHandler(ICB_PresetColor.ColorSelectedEvent, new RoutedEventHandler(OnColorSelected));
+        }
+        private void OnColorSelected(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource is ICB_PresetColor selector)
+            {
+                ColorPreview.Fill = new SolidColorBrush(selector.Color);
+            }
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
