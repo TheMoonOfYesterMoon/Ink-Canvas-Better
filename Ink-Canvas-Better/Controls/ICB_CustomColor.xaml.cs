76e356433df57ebf354ecf577a9cab2de0834a41
BaiYang2238 <142558485+BaiYang2238@users.noreply.github.com>
2025-07-18 00:54:19 +0800

    [refactor] part 20

--- Ink-Canvas-Better/Controls/ICB_CustomColor.xaml.cs
+++ Ink-Canvas-Better/Controls/ICB_CustomColor.xaml.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ColorConverter = Ink_Canvas_Better.Helpers.Others.ColorConverter;
 
 namespace Ink_Canvas_Better.Controls
 {
+
     /// <summary>
     /// ICB_CustomColor.xaml 的交互逻辑
     /// </summary>
     public partial class ICB_CustomColor : UserControl
     {
+        bool _IsFirstSetColor = true;
+        bool _temp = false;
+
         public ICB_CustomColor()
         {
             InitializeComponent();
@@ -47,6 +47,20 @@ namespace Ink_Canvas_Better.Controls
         {
             var control = (ICB_CustomColor)dependencyObject;
             control.InnerBorder.Background = new SolidColorBrush((Color)eventArgs.NewValue);
+            if (control._IsFirstSetColor)
+            {
+                control._IsFirstSetColor = false;
+                control.ColorPicker.SelectedColor = (Color)eventArgs.NewValue;
+                ColorConverter.ColorToRgb((Color)eventArgs.NewValue, out byte r, out byte g, out byte b);
+                control.R.Value = r;
+                control.G.Value = g;
+                control.B.Value = b;
+                ColorConverter.ColorToHsl((Color)eventArgs.NewValue, out double h, out double s, out double l);
+                control.H.Value = h;
+                control.S.Value = s;
+                control.L.Value = l;
+                control.Hex.Text = ColorConverter.ColorToHex((Color)eventArgs.NewValue);
+            }
         }
 
         #endregion
@@ -119,7 +133,77 @@ namespace Ink_Canvas_Better.Controls
         {
             Color = ColorPicker.SelectedColor;
             Button_Click(sender, e);
+            _temp = true;
+
+            ColorConverter.ColorToRgb(Color, out byte r, out byte g, out byte b);
+            R.Value = r;
+            G.Value = g;
+            B.Value = b;
+            ColorConverter.ColorToHsl(Color, out double h, out double s, out double l);
+            H.Value = h;
+            S.Value = s;
+            L.Value = l;
+            Hex.Text = ColorConverter.ColorToHex(Color);
+
+            _temp = false;
         }
 
+        private void NumberBox_ValueChanged(iNKORE.UI.WPF.Modern.Controls.NumberBox sender, iNKORE.UI.WPF.Modern.Controls.NumberBoxValueChangedEventArgs args)
+        {
+            if (!_temp)
+            {
+                switch ((String)sender.Tag)
+                {
+                    case "RGB":
+                    {
+                        Color = ColorConverter.RgbToColor((byte)R.Value, (byte)G.Value, (byte)B.Value);
+
+                        ColorPicker.SelectedColor = Color;
+                        ColorConverter.ColorToHsl(Color, out double h, out double s, out double l);
+                        H.Value = h;
+                        S.Value = s;
+                        L.Value = l;
+                        Hex.Text = ColorConverter.ColorToHex(Color);
+                        break;
+                    }
+                    case "HSL":
+                    {
+                        Color = ColorConverter.HslToColor(H.Value, S.Value, L.Value);
+
+                        ColorPicker.SelectedColor = Color;
+                        ColorConverter.ColorToRgb(Color, out byte r, out byte g, out byte b);
+                        R.Value = r;
+                        G.Value = g;
+                        B.Value = b;
+                        Hex.Text = ColorConverter.ColorToHex(Color);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // TODO
+            if (!Regex.IsMatch(((TextBox)sender).Text, @"^#?([0-9a-fA-F]{6})$"))
+            {
+                Hex.Text = "000000";
+                throw new FormatException("Invalid Hex Color Format. Expected format: #RRGGBB or RRGGBB.");
+            }
+            else
+            {
+                Color = ColorConverter.HexToColor(Hex.Text);
+
+                ColorPicker.SelectedColor = Color;
+                ColorConverter.ColorToRgb(Color, out byte r, out byte g, out byte b);
+                R.Value = r;
+                G.Value = g;
+                B.Value = b;
+                ColorConverter.ColorToHsl(Color, out double h, out double s, out double l);
+                H.Value = h;
+                S.Value = s;
+                L.Value = l;
+            }
+        }
     }
 }
