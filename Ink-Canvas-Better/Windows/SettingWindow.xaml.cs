be397cf4c65e53ed485407dfddd23a16b2c4f1c5
BaiYang2238 <142558485+BaiYang2238@users.noreply.github.com>
2025-08-21 16:23:02 +0800

    [refactor] part 60

--- Ink-Canvas-Better/Windows/SettingWindow.xaml.cs
+++ Ink-Canvas-Better/Windows/SettingWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Ink_Canvas_Better.Pages.SettingPages;
+using System;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using iUWM = iNKORE.UI.WPF.Modern;
 
 namespace Ink_Canvas_Better.Windows
 {
@@ -13,5 +15,23 @@ namespace Ink_Canvas_Better.Windows
             InitializeComponent();
         }
 
+        private void Navigation_SelectionChanged(iUWM.Controls.NavigationView sender, iUWM.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+            switch (((iUWM.Controls.NavigationViewItem)sender.SelectedItem).Name)
+            {
+                case "SettingNaviagtion_Item_Home":
+                    SettingFrame.Navigate(new Home());
+                    break;
+                case "SettingNaviagtion_Item_StartupAndUpdate":
+                    break;
+                case "SettingNaviagtion_Item_Appearance":
+                    SettingFrame.Navigate(new Appearance());
+                    break;
+                case "SettingNaviagtion_Item_PPT":
+                    break;
+                case "SettingNaviagtion_Item_ExperimentalFeatures":
+                    break;
+            }
+        }
     }
 }
