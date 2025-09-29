3a20bca29bab7c08896ab6c88f80e67ae370bd0e
ThreeMonthAgo <225839283+ThreeMonthAgo@users.noreply.github.com>
2025-09-29 12:10:06 +0800

    [change]moved to .NET 8.0

---
+++ Ink-Canvas-Better.Controls/Helpers/Converter/BooleanToTextCconverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Ink_Canvas_Better.Controls.Helpers.Converter
+{
+    [Localizability(LocalizationCategory.NeverLocalize)]
+    public sealed class BooleanToTextCconverter_Pin : IValueConverter
+    {
+        public BooleanToTextCconverter_Pin()
+        {
+
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool b;
+            if (value == null)
+            {
+                b = false;
+            }
+            else
+            {
+                b = (bool)value;
+            }
+            return b ? "\ue77a" : "\ue718"; // Pinned : Unpinned
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is String s)
+            {
+                if (s == "\ue77a")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Ink-Canvas-Better.Controls/Helpers/ThemeHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Ink_Canvas_Better.Controls.Helpers
+{
+    public static class ThemeHelper
+    {
+        public static ResourceDictionary Dictionary { get; set; }
+        static ThemeHelper()
+        {
+            Dictionary = new ResourceDictionary()
+            {
+                Source = new Uri("pack://application:,,,/Ink_Canvas_Better.Controls;component/Themes/Generic.xaml", UriKind.RelativeOrAbsolute)
+            };
+        }
+
+        #region Fonts
+
+        public static FontFamily GetFont(string key)
+        {
+            if (Dictionary.Contains(key) && Dictionary[key] is FontFamily fontFamily)
+            {
+                return fontFamily;
+            }
+            return new FontFamily(key);
+        }
+
+        public static FontFamily SegoeFluentIcons => GetFont("SegoeFluentIcons");
+
+        #endregion
+
+        #region Colors
+
+        public static Brush GetBrush(string key)
+        {
+            if (Dictionary.Contains(key) && Dictionary[key] is Brush brush)
+            {
+                return brush;
+            }
+            return Brushes.Transparent;
+        }
+
+        public static Brush DefaultBackgroundColor => GetBrush("DefaultBackgroundColor");
+        public static Brush DefaultBackgroundColor_Opacity => GetBrush("DefaultBorderColor");
+        public static Brush DefaultForegroundColor => GetBrush("DefaultBorderColor");
+        public static Brush DefaultBorderColor => GetBrush("DefaultBorderColor");
+        public static Brush DefaultButtonHoverColor => GetBrush("DefaultHoverColor");
+
+        #endregion
+    }
+}
